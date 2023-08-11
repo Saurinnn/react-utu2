a2b33525e5dc487f881c15ea415f3db1818abff7
Saurinnn <44335507+Saurinnn@users.noreply.github.com>
2023-08-11 13:26:36 +0900

    スマホ用CSS編集

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
+
 const Dropdown = ({ order ,text, addCount }) => {
   return (
     <div>
       <h3>あと{20-order}問/全20問</h3>
       <p className="pt-5 pb-7 text-3xl sm:w-6/12 md:w-9/12 lg:w-1/3 mr-auto ml-auto rounded-lg bg-blue-100 text-gray-700">{text}</p>
-      <select className="mt-7 mb-7 h-10 w-80" id="freq" onChange={addCount}>
+      <select className="mt-7 mb-7 h-10 w-80 dropdown-select" id="freq" onChange={addCount}>
         <option value="" selected>以下から選択してください</option>
         <option value="0">滅多にない～全くない（週0-1日）</option>
         <option value="1">いくらか～少しある（週1-2日）</option>
